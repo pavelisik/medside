a6620de5e221679654e268d42c5c7e894e21e32a
Pavel Lisitsky <pavelisik@gmail.com>
2025-01-27 22:13:57 +0300

    demo probnik

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Post from './components/Post';
 import './App.css';
 
 function App() {
-    return <div className="App"></div>;
+    const [posts, setPosts] = useState([]);
+
+    // const fetchPosts = async () => {
+    //     const res = await axios.get(
+    //         'https://medside.ru/wp-json/wp/v2/posts/162120'
+    //     );
+    // };
+
+    const fetchPosts = () => {
+        axios.get('https://medside.ru/wp-json/wp/v2/posts').then((res) => {
+            setPosts(res.data);
+        });
+    };
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
+    return (
+        <div className="App">
+            {posts.map((item, key) => (
+                <Post post={item} key={item.id} />
+            ))}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Post.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Post({ post }) {
+    const [featuredImage, setFeaturedImage] = useState();
+    const [postDate, setPostDate] = useState(null);
+
+    const getImage = async () => {
+        try {
+            const response = await axios.get(
+                post?._links['wp:attachment'][0]?.href
+            );
+
+            if (response.data && response.data[0].source_url) {
+                setFeaturedImage(response.data.pop().source_url);
+            }
+
+            if (post.date) {
+                const dateObj = new Date(post.date);
+                const formatedDate = dateObj.toLocaleDateString('en-US', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                });
+                setPostDate(formatedDate);
+            }
+        } catch (error) {
+            console.log('Error - ', error);
+        }
+    };
+
+    useEffect(() => {
+        getImage();
+    }, [post]);
+
+    return (
+        <div>
+            {featuredImage && (
+                <img src={featuredImage} alt={post.title.rendered} />
+            )}
+            <h2>{post.title.rendered}</h2>
+            <div>{postDate}</div>
+            <div dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }} />
+        </div>
+    );
+}
+
+export default Post;
