e14e425a70668ed152a710a16465ae43fce987cb
Pavelisik <pavelisik@gmail.com>
2025-06-01 10:08:02 +0300

    clear some files

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
-import MainLayout from './layouts/MainLayout';
-import MainPage from './pages/Home/MainPage';
+import MainLayout from './layouts/MainLayout'
+import MainPage from './pages/Home/MainPage'
 
 function App() {
-    return (
-        <MainLayout>
-            <MainPage />
-        </MainLayout>
-    );
+  return (
+    <MainLayout>
+      <MainPage />
+    </MainLayout>
+  )
 }
 
-export default App;
-
-// КОД ДЛЯ ТЕСТОВ:
-
-// import './App.css';
-// import TestContainer from './test/components/TestContainer';
-
-// function App() {
-//     return (
-//         <div className="App">
-//             <TestContainer />
-//         </div>
-//     );
-// }
-
-// export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
-import './assets/styles/global.css';
-// в index можно подключить tailwind, а reset это потом когда буду на модули переходить надо будет проверить и применить по идее
-// import './index.css';
-// import './reset.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+import './assets/styles/global.css'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root')! as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root')! as HTMLElement
+)
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-);
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
